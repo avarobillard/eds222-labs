3c03cf41c59310a669b1cbc68434f90ccacc3631
avarobillard <avamorganr@gmail.com>
2025-10-21 22:08:15 -0700

    Attempt bootstrapping

---
+++ week3/interactive.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(palmerpenguins)
+
+# bill length ~ interaction of body mass and sex
+penguin_int_mod <- lm(bill_length_mm ~ body_mass_g + sex + body_mass_g:sex,
+                      data = penguins)
+summary(penguin_int_mod)
+
+# short hand for interactions
+penguin_int_mod2 <- lm(bill_length_mm ~ body_mass_g * sex, penguins)
